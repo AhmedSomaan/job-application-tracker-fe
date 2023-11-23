191824214642238bbd440cb0600dd20647a0ad91
Ahmed Somaan <ahmed_somaan@hotmail.com>
2023-11-23 17:55:00 -0500

    initial commit

---
+++ src/pages/HomePage.tsx
@@ -0,0 +1,16 @@
+
+const HomePage = () => {
+  return (
+    <div>
+      <header>
+        <nav>
+          <img/>
+          <a href="">Bookmarks</a>
+        </nav>
+      </header>
+      <h1>Search for Job postings</h1>
+    </div>
+  );
+}
+
+export default HomePage
