494b4f98ad8d29f3dc825948867c67a958653f2c
Ahmed Somaan <ahmed_somaan@hotmail.com>
2023-11-23 23:01:44 -0500

    add icons and make homepage

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,16 +1,45 @@
-
+import { useState, ChangeEvent, KeyboardEvent } from "react";
+import bookmarkIcon from "../assets/icons/BookmarkNavIcon.svg";
+import { useNavigate, NavLink } from "react-router-dom";
 const HomePage = () => {
+  const navigate = useNavigate();
+  const [searchTerm, setSearchTerm] = useState<string>("");
+
+  const handleInputChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setSearchTerm(e.target.value);
+  };
+
+  const handleKeyPress = (e: KeyboardEvent<HTMLInputElement>) => {
+    if (e.key === "Enter") {
+      console.log("Navigate to job postings for search term:", searchTerm);
+      navigate("/");
+    }
+  };
   return (
-    <div>
+    <div className="min-h-screen">
       <header>
-        <nav>
-          <img/>
-          <a href="">Bookmarks</a>
+        <nav className="flex justify-end p-4 items-center">
+          <NavLink to={"/"} className="flex items-center gap-4 text-2xl">
+            Bookmarks
+            <img src={bookmarkIcon} alt="bookmark icon" />
+          </NavLink>
         </nav>
       </header>
-      <h1>Search for Job postings</h1>
+      <main className="p-4 h-full">
+        <h1 className="text-4xl font-medium mb-10">Search for Job postings</h1>
+        <div>
+          <input
+            className="placeholder-white px-6 py-4 rounded-xl w-full bg-dark-grey drop-shadow-md bg-[url('/src/assets/icons/SearchIcon.svg')] bg-no-repeat bg-[center_right_2rem]"
+            type="text"
+            placeholder="Search..."
+            value={searchTerm}
+            onChange={handleInputChange}
+            onKeyDown={handleKeyPress}
+          />
+        </div>
+      </main>
     </div>
   );
-}
+};
 
-export default HomePage
+export default HomePage;
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,11 @@ export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {},
+    colors: {
+      white: "#ffffff",
+      "dark-grey": "#171717",
+      "light-grey": "#353535",
+    },
   },
   plugins: [],
 };
