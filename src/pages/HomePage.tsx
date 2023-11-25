3cce7bd2411b3acc30a3a53a923a40d51692ee7e
Ahmed Somaan <ahmed_somaan@hotmail.com>
2023-11-24 19:05:34 -0500

    add floating cat

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,5 +1,6 @@
 import { useState, ChangeEvent, KeyboardEvent } from "react";
 import bookmarkIcon from "../assets/icons/BookmarkNavIcon.svg";
+import cat from "../assets/cat_on_planet.png";
 import { useNavigate, NavLink } from "react-router-dom";
 const HomePage = () => {
   const navigate = useNavigate();
@@ -16,26 +17,39 @@ const HomePage = () => {
     }
   };
   return (
-    <div className="min-h-screen">
+    <div className="flex min-h-screen flex-col">
       <header>
-        <nav className="flex justify-end p-4 items-center">
-          <NavLink to={"/"} className="flex items-center gap-4 text-2xl">
+        <nav className="flex items-center justify-end p-4 md:px-8">
+          <NavLink
+            to={"/"}
+            className="flex items-center gap-4 text-xl md:text-2xl"
+          >
             Bookmarks
             <img src={bookmarkIcon} alt="bookmark icon" />
           </NavLink>
         </nav>
       </header>
-      <main className="p-4 h-full">
-        <h1 className="text-4xl font-medium mb-10">Search for Job postings</h1>
+      <main className="h-full p-4 md:px-8 grow flex flex-col">
+        <h1 className="mb-8 text-2xl font-medium sm:text-3xl md:text-4xl">
+          Search for Job postings
+        </h1>
         <div>
           <input
-            className="placeholder-white px-6 py-4 rounded-xl w-full bg-dark-grey drop-shadow-md bg-[url('/src/assets/icons/SearchIcon.svg')] bg-no-repeat bg-[center_right_2rem]"
+            className="w-full max-w-5xl rounded-xl bg-dark-grey bg-[url('/src/assets/icons/SearchIcon.svg')] bg-[center_right_2rem] bg-no-repeat px-6 py-4 placeholder-white drop-shadow-md"
             type="text"
             placeholder="Search..."
             value={searchTerm}
             onChange={handleInputChange}
             onKeyDown={handleKeyPress}
           />
+        </div>
+        <div className="flex flex-col justify-center items-center h-full grow">
+          <img
+            className="animate-float w-[10rem] md:w-[14rem]"
+            src={cat}
+            alt="Green Cat floating on planet"
+          />
+          <div className="w-[10rem] h-4 bg-light-grey rounded-[50%] opacity-40 animate-shadow"></div>
         </div>
       </main>
     </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,22 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      keyframes: {
+        float: {
+          "0%, 100%": { transform: "translateY(0)" },
+          "50%": { transform: "translateY(-1.5rem)" },
+        },
+        shadow: {
+          "0%, 100%": { transform: "scaleX(1)", opacity: "0.4"},
+          "50%": { transform: "scaleX(0.85)", opacity: "0.2"},
+        },
+      },
+      animation: {
+        float: "float 6s ease-in-out infinite",
+        shadow: "shadow 6s ease-in-out infinite",
+      },
+    },
     colors: {
       white: "#ffffff",
       "dark-grey": "#171717",
